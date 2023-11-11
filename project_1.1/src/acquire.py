9ac199eddcec5c02bf00faa683b3126968b966de
thmagqa022 <thmagqa022@student.wethinkcode.co.za>
2023-11-11 19:31:56 +0200

    Added logging

--- project_1.1/src/acquire.py
+++ project_1.1/src/acquire.py
@@ -5,6 +5,29 @@ import json
 import sys
 import csv
 from pathlib import Path
+import logging
+
+# Create and configure a logger
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+# Create a file handler
+handler = logging.FileHandler("app.log")
+handler.setLevel(logging.INFO)
+
+# Create a console handler with a higher log level
+console_handler = logging.StreamHandler()
+console_handler.setLevel(logging.ERROR)
+
+# Create a formatter and set it for the handlers
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+handler.setFormatter(formatter)
+console_handler.setFormatter(formatter)
+
+# Add the handlers to the logger
+logger.addHandler(handler)
+logger.addHandler(console_handler)
+
 
 def get_options(argv: list[str]) -> argparse.Namespace:
     """
@@ -42,7 +65,8 @@ def main(argv: list[str]) -> None:
         options.output / "Series_4.ndjson",
     ]
 
-    try: 
+    try:
+        print(f"Extracting data from {argv[-1]}...")
         target_files = [
             target.open("w") for target in targets
         ]
@@ -55,8 +79,12 @@ def main(argv: list[str]) -> None:
         
         for target in target_files:
             target.close()
-    except FileNotFoundError:
-        print("File not found: "+ argv[-1])
+        
+        print(f"Data successfully extracted to {argv[-2]}/Series_1.ndjson, {argv[-2]}/Series_2.ndjson, and so on.")
+        print("Extraction complete.")
+    except FileNotFoundError as e:
+        logger.error(f"File not found: {e}")
+        print(f"File not found: {e}")
 
 if __name__ == "__main__":
     main(sys.argv[1:])
